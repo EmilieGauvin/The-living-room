193fb8d6939935f2ff87d6903648de2ab5274095
EmilieGauvin <emiliegvn@gmail.com>
2022-12-02 16:45:02 +0100

    update2 021222

--- src/LoadingAnim/LoadingAnim.jsx
+++ src/LoadingAnim/LoadingAnim.jsx
@@ -1,5 +1,5 @@
 import "./LoadingAnim.css";
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 
 export default function LoadingAnim() {
@@ -28,9 +28,6 @@ export default function LoadingAnim() {
               </div>
             </div>
         </div>
-        {/* <div className={showInstuction === false ? 'instruction hideInstuction' : 'instruction showInstuction'}> 
-        <p><i>{english === true ? 'Grab the shapes and match them to the drawing' : 'Déplace les formes dans les dessins correspondants'}</i></p>
-        </div> */}
   </div>
 );
 }
